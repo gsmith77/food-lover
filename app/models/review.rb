89da97a4f61178e9711cde38ac12ac2bae9ddebf
gsmith77 <grantsmith1256@gmail.com>
2019-09-20 11:23:16 -0700

    deleted unneccesary class methods

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,15 +2,4 @@ class Review < ApplicationRecord
     belongs_to :business
     belongs_to :user
 
-    def business_attributes=(business_attributes)
-        business_attributes.each do |i, business_attribute|
-           review = self.review.build(business_attribute)
-        end
-    end
-
-    def user_attributes=(user_attributes)
-        user_attributes.each do |i, user_attribute|
-            review = self.review.build(user_attribute)
-        end
-    end
 end
