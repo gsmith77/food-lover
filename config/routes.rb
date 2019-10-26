3329c61a52074883dc83f680eb54dcc8d8320673
gsmith77 <grantsmith1256@gmail.com>
2019-10-25 22:05:56 -0500

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   namespace 'api' do
     namespace 'v1' do
       get '/search_businesses', to: 'fetch#search_businesses'
