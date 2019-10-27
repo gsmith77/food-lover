67a0975caa86b67dba32c24c0fca224094a6f2f0
gsmith77 <grantsmith1256@gmail.com>
2019-10-27 02:34:36 -0500

    Updates

---
+++ precache-manifest.0382627f714c4710057c0cb9595bd607.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "694b5a9cb2c074982e799b72ebabbe19",
+    "url": "/yelp-clone/index.html"
+  },
+  {
+    "revision": "114750b62e05ad7305dd",
+    "url": "/yelp-clone/static/css/main.278f3ba2.chunk.css"
+  },
+  {
+    "revision": "03893520d1096f78fca1",
+    "url": "/yelp-clone/static/js/2.38d68af0.chunk.js"
+  },
+  {
+    "revision": "114750b62e05ad7305dd",
+    "url": "/yelp-clone/static/js/main.f6d698ba.chunk.js"
+  },
+  {
+    "revision": "2ef9d4e562a8501fc0b2",
+    "url": "/yelp-clone/static/js/runtime~main.a51639b2.js"
+  }
+]);
